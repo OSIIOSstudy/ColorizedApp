46f26ed935e07e84536cb0785e7efeef9e586e9c
OSIIOSdev <OSIIOS.dev@gmail.com>
2024-03-12 00:36:37 -0700

    1. Sliders and view configured

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var colorizedView: UIView!
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
@@ -19,6 +21,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupColorizedView()
+        
+        showColorResult(label: redValueLabel, slider: redSlider)
+        showColorResult(label: greenValueLabel, slider: greenSlider)
+        showColorResult(label: blueValueLabel, slider: blueSlider)
+    }
+    
+    @IBAction func showRedValue() {
+        showColorResult(label: redValueLabel, slider: redSlider)
+    }
+    
+    @IBAction func showGreenValue() {
+        showColorResult(label: greenValueLabel, slider: greenSlider)
+    }
+    
+    @IBAction func showBlueValue() {
+        showColorResult(label: blueValueLabel, slider: blueSlider)
+    }
+    
+    private func setupColorizedView() {
+        colorizedView.layer.cornerRadius = 10
+        colorizedView.layer.borderWidth = 2
+        colorizedView.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+    
+    private func showColorResult(label: UILabel, slider: UISlider) {
+        label.text = String(format: "%.0f", slider.value)
+        
+        colorizedView.backgroundColor = UIColor(red: CGFloat(redSlider.value / 100),
+                                                green: CGFloat(greenSlider.value / 100),
+                                                blue: CGFloat(blueSlider.value / 100), alpha: 1)
     }
 }
 
